68a614e2c52db7c292b35de9d5073d7f3f86bc12
机器人总动员 <73592731+devourbots@users.noreply.github.com>
2021-05-05 16:27:21 +0800

    init

--- func.py
+++ func.py
@@ -20,6 +20,8 @@ def chat_content_exec(update, context):
     r = connector.get_connection()
     text = update.message.text
     chat_type = update.effective_chat.type
+    if chat_type != "supergroup":
+        return
     user_id = update.effective_user.id
     chat_id = update.effective_message.chat_id
     try:
@@ -27,12 +29,13 @@ def chat_content_exec(update, context):
     except Exception as e:
         username = update.effective_user.id
     print("\n---------------------------")
-    print("内容: " + text)
+    print("内容: " + text[:10])
     print("群组类型: " + str(chat_type))
     print("用户ID: " + str(user_id))
     print("chat_id: " + str(chat_id))
     if "/" in text:
         print("这是一条指令信息，跳过")
+        return
     else:
         if text[-1] not in ["，", "。", "！", "：", "？", "!", "?", ",", ":", "."]:
             r.append("{}_chat_content".format(chat_id), text + "。")
